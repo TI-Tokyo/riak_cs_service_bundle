dd5ff22a2d7a5d20dda01e573c280f1d89959077
Andriy Zavada <andriy.zavada@tiot.jp>
2023-12-27 14:10:53 +0200

    update copyright year

--- lib/bundle_lib.py
+++ lib/bundle_lib.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2021 TI Tokyo    All Rights Reserved.
+# Copyright (c) 2021-2023 TI Tokyo    All Rights Reserved.
 #
 # This file is provided to you under the Apache License,
 # Version 2.0 (the "License"); you may not use this file
